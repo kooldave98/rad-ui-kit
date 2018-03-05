65e594aa98989292d9176aa9165a108fac8766a8
David Olubajo <david.olubajo@davidolubajo.com>
2018-03-05 12:43:31 +0000

    create sample to demonstrate usage

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,41 +2,46 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 import { Editor } from '../../src';
-import { Required } from '../../src/FieldValidators';
+import { FieldValidators } from '../../src';
+
+let standardInputField = ({ meta, value, errors, handleChange, validate, key }) => {
+  return (
+    <div key={key}>
+      <label>{meta.label}</label>
+      <input onChange={e => handleChange(e.target.value)} />
+      {!!meta.helpMessage &&
+        <span>{meta.helpMessage}</span>}
+      {errors.map((e, i) => (
+        <p className="help-block" key={i}>{e}</p>
+      ))}
+    </div>
+  );
+};
 
 let fields = [
   {
-    name: "field1",
+    name: "firstname",
     defaultValue: "",
-    label: "Field1",
-    helpMessage: "This should be a short message that helps the user",
-    validators: [Required]
+    label: "First Name",
+    helpMessage: "Please enter your first name here",
+    validators: [FieldValidators.Required],
+    options: { "option1": "123", "option2": "456" },
+    render: standardInputField
   },
-  // {
-  //   name: "field1",
-  //   defaultValue: "",
-  //   label: "Field1",
-  //   helpMessage: "This should be a short message that helps the user",
-  //   validators: [],
-  //   options: { "option1": "123", "option2": "456" },
-  //   render: ({ meta, value, errors, handleChange, validate, key }) => {
-  //     return (
-  //       <div key={key}>
-  //         <label>{meta.label}</label>
-  //         <input onChange={e => handleChange(e.target.value)} />
-  //         {errors.map((e, i) => (
-  //           <p className="help-block" key={i}>{e}</p>
-  //         ))}
-  //         {!!meta.helpMessage &&
-  //           <span title={`${meta.label} - help`} message={meta.helpMessage} />}
-  //       </div>
-  //     );
-  //   }
-  //}
+  {
+    name: "lastname",
+    defaultValue: "",
+    label: "Last Name",
+    helpMessage: "Please enter your surname here",
+    validators: [FieldValidators.Required],
+    options: { "option1": "123", "option2": "456" },
+    render: standardInputField
+  }
 ];
 
 let onSubmit = (data, handleServerErrors) => {
-  handleServerErrors({ field1: ["Server error"] });
+  console.log(data);
+  //handleServerErrors({ field1: ["Server error"] });
 };
 
 class Demo extends Component {
